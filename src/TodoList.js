294a87004b37029b1e12742302182fab24834aed
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-12-20 11:40:53 +0000

    updated last to a new function

--- src/TodoList.js
+++ src/TodoList.js
@@ -11,22 +11,25 @@ class TodoList {
     return item
   }
 
+  formatTextOverflow() {
+    const reducedText = [...this.items]
+
+    reducedText.forEach((item) => {
+      if (item.text.length >= 20) {
+        console.log('item is bigger than 20')
+        item.text = item.text.slice(0, 17) + '...'
+      }
+    })
+
+    return reducedText
+  }
+
   showAll() {
     if (this.items.length === 1) {
       return this.items
     }
 
-    const reducedText = [...this.items]
-    reducedText.forEach((item) => {
-      if (item.text.length >= 20) {
-        console.log('item is bigger than 20')
-        item.text = item.text.slice(0, 17)
-        item.text += '...'
-      }
-    })
-
-    console.log(reducedText)
-    return reducedText
+    return this.formatTextOverflow()
   }
 
   setComplete(id) {
