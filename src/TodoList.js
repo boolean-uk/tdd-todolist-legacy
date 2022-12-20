10868769d9640db74581cf3be03c59e72fde5629
Joel Watkins <eck087@yahoo.co.uk>
2022-12-20 11:12:08 +0000

    added tests and functions for limiting todo text to 20 chars

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,40 +1,53 @@
 class TodoList {
-  constructor () {
+  constructor() {
     this.id = 0
     this.items = []
   }
 
-  create (str) {
+  create(str) {
     this.id++
     const item = { id: this.id, text: str, status: 'incomplete' }
     this.items.push(item)
     return item
   }
 
-  showAll () {
-    return this.items
+  showAll() {
+    if (this.items.length === 1) return this.items
+    return this.limitTextTo20Chars()
   }
 
-  setComplete (id) {
+  setComplete(id) {
     const item = this.findBy(id)
     item.status = 'complete'
     return item
   }
 
-  getByStatus (status) {
-    return this.items.filter(item => item.status === status)
+  getByStatus(status) {
+    return this.items.filter((item) => item.status === status)
   }
 
-  findBy (id) {
-    const item = this.items.find(item => item.id === id)
+  findBy(id) {
+    const item = this.items.find((item) => item.id === id)
     if (item === undefined) throw new Error('Item not found')
     return item
   }
 
-  deleteBy (id) {
+  deleteBy(id) {
     const item = this.findBy(id)
     const index = this.items.indexOf(item)
     return this.items.splice(index, 1)[0]
+  }
+
+  limitTextTo20Chars() {
+    const limitedTextTodos = this.items.map((item) => {
+      if (item.text.length > 20) {
+        return { ...item, text: `${item.text.substring(0, 20)}...` }
+      } else {
+        return { ...item }
+      }
+    })
+
+    return limitedTextTodos
   }
 }
 
