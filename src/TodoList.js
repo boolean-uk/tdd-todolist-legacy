1782140440ead063d43ebdffeaab05013d90ce6d
desokan <thefdmm@gmail.com>
2023-06-16 18:14:24 +0100

    completed core task

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,37 +1,61 @@
 class TodoList {
-  constructor () {
+  constructor() {
     this.id = 0
     this.items = []
   }
 
-  create (str) {
+  create(str) {
     this.id++
-    const item = { id: this.id, text: str, status: 'incomplete' }
+    const item = {
+      id: this.id,
+      text: str,
+      status: 'incomplete',
+      date: new Date().toLocaleDateString('en-GB')
+    }
     this.items.push(item)
     return item
   }
 
-  showAll () {
-    return this.items
+  showAll() {
+    const copyOfItems = [...this.items]
+    for (let i = 0; i <= this.items.length - 1; i++) {
+      if (this.items[i].text.length > 20) {
+        copyOfItems[i].text = copyOfItems[i].text.substring(0, 20) + '...'
+      }
+    }
+    return copyOfItems
   }
 
-  setComplete (id) {
+  setComplete(id) {
     const item = this.findBy(id)
     item.status = 'complete'
     return item
   }
 
-  getByStatus (status) {
-    return this.items.filter(item => item.status === status)
+  getByStatus(status) {
+    const copyOfItems = [...this.items]
+    for (let i = 0; i <= this.items.length - 1; i++) {
+      if (this.items[i].text.length > 20) {
+        copyOfItems[i].text = copyOfItems[i].text.substring(0, 20) + '...'
+      }
+    }
+    return copyOfItems.filter((item) => item.status === status)
   }
 
-  findBy (id) {
-    const item = this.items.find(item => item.id === id)
+  findBy(id) {
+    const item = this.items.find((item) => item.id === id)
     if (item === undefined) throw new Error('Item not found')
     return item
   }
 
-  deleteBy (id) {
+  findByDate(date) {
+    const item = this.items.find((item) => item.date === date)
+    if (item === undefined) return []
+
+    return item
+  }
+
+  deleteBy(id) {
     const item = this.findBy(id)
     const index = this.items.indexOf(item)
     return this.items.splice(index, 1)[0]
@@ -39,3 +63,13 @@ class TodoList {
 }
 
 module.exports = TodoList
+
+const todoList = new TodoList()
+todoList.create(
+  'The North-West National Vice Chairman of the All Progressives Congress, Salihu Lukman, has raised the alarm that the National Chairman, Senator Abdullahi Adamu, is running the ruling party like an army barrack without recourse to the National Working Committee.'
+)
+
+todoList.create(
+  'This was even as the Kaduna politician expressed concerns that the day-to-day administration of the secretariat and key decisions that involve party activities are taken at the discretion of the national chairman.'
+)
+console.log(todoList.showAll())
---
+++ src/try.js
@@ -0,0 +1,6 @@
+const fiver = [1, 2, 3, 4, 5, 'seven', 'eight']
+const sixer = [{ firstName: 'ade' }, { lastName: 'Adesokan' }]
+
+console.log(...fiver)
+console.log(...sixer)
+
