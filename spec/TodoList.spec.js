b7cf14204fbe3e28518b21bccb7906d71df4760f
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-06-16 18:24:17 +0200

    Finished core

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -2,9 +2,11 @@ const TodoList = require("../src/TodoList.js")
 
 describe("TodoList", () => {
   let todoList
+  let date
 
   beforeEach(() => {
     todoList = new TodoList()
+    date = new Date()
   })
 
   it("creates a todo item", () => {
@@ -12,7 +14,8 @@ describe("TodoList", () => {
     const expected = {
       id: 1,
       text: "turn the heating on!",
-      status: "incomplete"
+      status: "incomplete",
+      date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()
     }
 
     // execute
@@ -22,27 +25,38 @@ describe("TodoList", () => {
     expect(result).toEqual(expected)
   })
 
+
   it("returns all items", () => {
     // set up
     const item1 = {
       id: 1,
       text: "turn the heating on!",
-      status: "incomplete"
+      status: "incomplete",
+      date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()
     }
     const item2 = {
       id: 2,
       text: "Do the washing up",
-      status: "incomplete"
+      status: "incomplete",
+      date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()
     }
-    const expected = [item1, item2]
+    const item3 = {
+      id: 3,
+      text: "This is a todo that ...",
+      status: 'incomplete',
+      date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()
+    }
+    const expected = [item1, item2, item3]
 
     // execute
     todoList.create("turn the heating on!")
     todoList.create("Do the washing up")
+    todoList.create('This is a todo that is longer than 20 characters')
 
     // verify
     expect(todoList.showAll()).toEqual(expected)
   })
+  
 
   it("sets item to be complete if found", () => {
     // set up
@@ -50,7 +64,8 @@ describe("TodoList", () => {
     const expected = {
       id: 1,
       text: "turn the heating on!",
-      status: "complete"
+      status: "complete",
+      date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()
     }
 
     // execute
@@ -70,9 +85,10 @@ describe("TodoList", () => {
   it("gets incomplete items", () => {
     // set up
     const item1 = todoList.create("turn the heating on!")
-    const item2 = todoList.create("Do the washing up")
+    todoList.create("Do the washing up")
+    todoList.create('This is a string that is longer than 20 characters')
     todoList.setComplete(item1.id)
-    const expected = [item2]
+    const expected = [{id: 2, text: "Do the washing up", status: "incomplete", date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()}, {id: 3, text: 'This is a string tha...', status: "incomplete", date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()}]
 
     // execute
     const result = todoList.getByStatus("incomplete")
@@ -84,9 +100,11 @@ describe("TodoList", () => {
   it("gets complete items", () => {
     // set up
     const item1 = todoList.create("turn the heating on!")
-    const item2 = todoList.create("Do the washing up")
+    todoList.create("Do the washing up")
+    const item3 = todoList.create('This is a string that is longer than 20 characters')
     todoList.setComplete(item1.id)
-    const expected = [item1]
+    todoList.setComplete(item3.id)
+    const expected = [{id: 1, text: "turn the heating on!", status: "complete", date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()}, {id: 3, text: "This is a string tha...", status: "complete", date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()}]
 
     // execute
     const result = todoList.getByStatus("complete")
@@ -101,7 +119,8 @@ describe("TodoList", () => {
     const expected = {
       id: 1,
       text: "turn the heating on!",
-      status: "incomplete"
+      status: "incomplete",
+      date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()
     }
 
     // execute
@@ -124,7 +143,8 @@ describe("TodoList", () => {
     const expected = {
       id: 1,
       text: "turn the heating on!",
-      status: "incomplete"
+      status: "incomplete",
+      date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()
     }
 
     // execute
@@ -140,5 +160,51 @@ describe("TodoList", () => {
 
     // execute, verify
     expect(() => todoList.deleteBy(1)).toThrowError("Item not found")
+  })
+
+  // Happy Path
+  it('Finds the items and returns them in an array, showing the items corresponding to the same date', () => {
+    // Set up
+    todoList.create("turn the heating on!")
+    todoList.create("Do the washing up")
+    todoList.create('This is a string that is longer than 20 characters')
+
+    // Execute
+    const result = todoList.showByDate(date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString())
+
+    // Verify
+    expect(result).toEqual([{
+      id: 1,
+      text: "turn the heating on!",
+      status: "incomplete",
+      date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()
+    }, 
+    {
+      id: 2,
+      text: "Do the washing up",
+      status: "incomplete",
+      date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()
+    }, 
+    {
+      id: 3,
+      text: "This is a string tha...",
+      status: "incomplete",
+      date: date.getFullYear().toString() + '-' +(date.getMonth()+1).toString() + '-'+ date.getDate().toString()
+    }])
+  })
+
+  // Sad Path
+  it('Will return an empty array if there are no todos with the corresponding date', () => {
+    // Set up
+    todoList.create("turn the heating on!")
+    todoList.create("Do the washing up")
+    todoList.create('This is a string that is longer than 20 characters')
+
+    // Execute
+    const result = todoList.showByDate("2023-3-16")
+
+    // Verify
+    expect(result).toEqual([])
+
   })
 })
