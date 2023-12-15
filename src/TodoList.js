1c8f15cc5c1d3039b35920afbd5ef1409619395d
Chloe Zermatten <chloezermatten@gmail.com>
2023-12-15 16:44:59 +0000

    add displaySummary()

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,37 +1,48 @@
 class TodoList {
-  constructor () {
+  constructor() {
     this.id = 0
     this.items = []
   }
 
-  create (str) {
+  create(str) {
     this.id++
     const item = { id: this.id, text: str, status: 'incomplete' }
     this.items.push(item)
     return item
   }
 
-  showAll () {
+  showAll() {
     return this.items
   }
 
-  setComplete (id) {
+  displaySummary() {
+    const itemsLength = this.items.length
+    if (itemsLength === 1) {
+      return this.items[0].text
+    }
+
+    const summary = this.items.map((i) => i.text.slice(0, 20) + '...')
+
+    return summary
+  }
+
+  setComplete(id) {
     const item = this.findBy(id)
     item.status = 'complete'
     return item
   }
 
-  getByStatus (status) {
-    return this.items.filter(item => item.status === status)
+  getByStatus(status) {
+    return this.items.filter((item) => item.status === status)
   }
 
-  findBy (id) {
-    const item = this.items.find(item => item.id === id)
+  findBy(id) {
+    const item = this.items.find((item) => item.id === id)
     if (item === undefined) throw new Error('Item not found')
     return item
   }
 
-  deleteBy (id) {
+  deleteBy(id) {
     const item = this.findBy(id)
     const index = this.items.indexOf(item)
     return this.items.splice(index, 1)[0]
