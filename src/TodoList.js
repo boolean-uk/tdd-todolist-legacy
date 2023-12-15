a16f8bf62d58893c2197f394e75c45669021654f
Kye <NovusAa12@proton.me>
2023-12-15 15:58:16 +0000

    update source code

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,37 +1,82 @@
+const date = new Date()
+const currentDate = `${date.getFullYear()}/${
+  date.getMonth() + 1
+}/${date.getDate()}`
+
 class TodoList {
-  constructor () {
+  constructor() {
     this.id = 0
     this.items = []
   }
 
-  create (str) {
+  updateText() {
+    if (this.items.length > 1) {
+      this.items.forEach((item) => {
+        item.text.length > 20
+          ? (item.text = item.text.slice(0, 20) + '...')
+          : item.text
+      })
+    }
+  }
+
+  create(str) {
     this.id++
-    const item = { id: this.id, text: str, status: 'incomplete' }
+    const item = {
+      id: this.id,
+      text: str,
+      status: 'incomplete',
+      created: currentDate
+    }
     this.items.push(item)
     return item
   }
 
-  showAll () {
-    return this.items
+  showAll() {
+    this.updateText()
+
+    if (this.items.length === 0) {
+      return 'TodoList is empty'
+    } else {
+      return this.items
+    }
   }
 
-  setComplete (id) {
+  setComplete(id) {
     const item = this.findBy(id)
     item.status = 'complete'
     return item
   }
 
-  getByStatus (status) {
-    return this.items.filter(item => item.status === status)
+  getByDate(date) {
+    this.updateText()
+
+    if (date === undefined) {
+      throw new Error('Please input a date')
+    } else if (typeof date !== 'string') {
+      throw new Error("Please input a valid date e.g. '2023/6/14'")
+    }
+
+    const isValid = this.items.some((item) => item.created === date)
+
+    if (isValid) {
+      return this.items.filter((item) => item.created === date)
+    } else {
+      return 'There are no Todos on that date'
+    }
   }
 
-  findBy (id) {
-    const item = this.items.find(item => item.id === id)
+  getByStatus(status) {
+    this.updateText()
+    return this.items.filter((item) => item.status === status)
+  }
+
+  findBy(id) {
+    const item = this.items.find((item) => item.id === id)
     if (item === undefined) throw new Error('Item not found')
     return item
   }
 
-  deleteBy (id) {
+  deleteBy(id) {
     const item = this.findBy(id)
     const index = this.items.indexOf(item)
     return this.items.splice(index, 1)[0]
