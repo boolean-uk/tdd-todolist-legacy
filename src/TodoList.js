d916196892c75647ddd7fd0c2841067977552528
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2023-12-17 21:21:00 +0100

    installed npm and also add the date,year and month logic

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,37 +1,47 @@
 class TodoList {
-  constructor () {
+  constructor() {
     this.id = 0
     this.items = []
   }
 
-  create (str) {
+  create(str) {
+    const getDate = new Date()
+    const getYear = getDate.getFullYear()
+    const getMonth = getDate.getMonth() + 1
+    const getDay = getDate.getDate()
+    const getDateString = `${getYear}-${getMonth}-${getDay}`
     this.id++
-    const item = { id: this.id, text: str, status: 'incomplete' }
+    const item = {
+      id: this.id,
+      text: str,
+      status: 'incomplete',
+      getDate: getDateString
+    }
     this.items.push(item)
     return item
   }
 
-  showAll () {
+  showAll() {
     return this.items
   }
 
-  setComplete (id) {
+  setComplete(id) {
     const item = this.findBy(id)
     item.status = 'complete'
     return item
   }
 
-  getByStatus (status) {
-    return this.items.filter(item => item.status === status)
+  getByStatus(status) {
+    return this.items.filter((item) => item.status === status)
   }
 
-  findBy (id) {
-    const item = this.items.find(item => item.id === id)
+  findBy(id) {
+    const item = this.items.find((item) => item.id === id)
     if (item === undefined) throw new Error('Item not found')
     return item
   }
 
-  deleteBy (id) {
+  deleteBy(id) {
     const item = this.findBy(id)
     const index = this.items.indexOf(item)
     return this.items.splice(index, 1)[0]
