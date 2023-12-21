ea62229e3616b6991c8cf7ac4ca55db2a6f081bb
Papi <test@Tests-MacBook-Air.local>
2023-12-21 15:44:34 +0100

    done

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -5,140 +5,240 @@ describe("TodoList", () => {
 
   beforeEach(() => {
     todoList = new TodoList()
+    setId(1)
   })
 
-  it("creates a todo item", () => {
+  it("create() creates a todo item", () => {
     // set up
     const expected = {
       id: 1,
       text: "turn the heating on!",
-      status: "incomplete"
+      status: "incomplete",
+      date: '15/12/2023'
     }
 
     // execute
-    const result = todoList.create("turn the heating on!")
+    const result = todoList.create(new TodoItem("turn the heating on!"))
+
+    // verify
+    expect(result).toEqual(expected)
+  })
+})
+
+  it("showAll() returns all items", () => {
+    // set up
+    const expected = [
+      {
+        id: 1,
+        text: "turn the heating on!",
+        status: "incomplete",
+        date: '15/12/2023'
+      },
+      {
+        id: 2,
+        text: "Do the washing up",
+        status: "incomplete",
+        date: '15/12/2023'
+      },
+      {
+        id: 3,
+        text: "Do the laundry at ex...",
+        status: "incomplete",
+        date: '15/12/2023'
+      }
+    ]
+
+
+    // execute
+    todoList.create(new TodoItem("turn the heating on!"))
+    todoList.create(new TodoItem("Do the washing up"))
+    todoList.create(new TodoItem("Do the laundry at exactly 9am"))
+    const result = todoList.showAll()
 
     // verify
     expect(result).toEqual(expected)
   })
 
-  it("returns all items", () => {
-    // set up
-    const item1 = {
-      id: 1,
-      text: "turn the heating on!",
-      status: "incomplete"
-    }
-    const item2 = {
-      id: 2,
-      text: "Do the washing up",
-      status: "incomplete"
-    }
-    const expected = [item1, item2]
+  describe('Set complete status', () => {
+    it("sets item to be 'complete' if found & status is currently 'incomplete'", () => {
+      // set up
+      const expected = {
+        id: 1,
+        text: "turn the heating on!",
+        status: "complete",
+        date: '15/12/2023'
+      }
 
-    // execute
-    todoList.create("turn the heating on!")
-    todoList.create("Do the washing up")
+      // execute
+      todoList.create(new TodoItem("turn the heating on!"))
+      const result = todoList.setComplete(1)
 
-    // verify
-    expect(todoList.showAll()).toEqual(expected)
-  })
+      // verify
+      expect(result).toEqual(expected)
+    })
 
-  it("sets item to be complete if found", () => {
-    // set up
-    const item1 = todoList.create("turn the heating on!")
-    const expected = {
-      id: 1,
-      text: "turn the heating on!",
-      status: "complete"
-    }
+    it("sets item to be 'incomplete' if found & status is currently 'complete'", () => {
+      // set up
+      const expected = {
+        id: 1,
+        text: "turn the heating on!",
+        status: "incomplete",
+        date: '15/12/2023'
+      }
 
-    // execute
-    const result = todoList.setComplete(item1.id)
+      // execute
+      todoList.create(new TodoItem("turn the heating on!"))
+      todoList.setComplete(1)
+      const result = todoList.setComplete(1)
 
-    // verify
-    expect(result).toEqual(expected)
-  })
+      // verify
+      expect(result).toEqual(expected)
+    })
 
-  it("throws error if not found", () => {
-    // set up
+    it("throws error if item is not found", () => {
+      // set up
 
-    // execute, verify
-    expect(() => todoList.setComplete(1)).toThrowError("Item not found")
+      // execute, verify
+      expect(() => todoList.setComplete(1)).toThrowError("Item not found")
+    })
   })
 
   it("gets incomplete items", () => {
     // set up
-    const item1 = todoList.create("turn the heating on!")
-    const item2 = todoList.create("Do the washing up")
-    todoList.setComplete(item1.id)
-    const expected = [item2]
+    describe('Get item by completion status', () => {
+      it("gets incomplete items", () => {
+        // set up
+        const item1 = todoList.create(new TodoItem("turn the heating on!"))
+        const item2 = todoList.create(new TodoItem("Do the washing up"))
+        todoList.setComplete(item1.id)
+        const expected = [item2]
+  
+        // execute
+        const result = todoList.getByStatus("incomplete")
+  
+        // verify
+        expect(result).toEqual(expected)
+      })
+  
+      it("gets complete items", () => {
+        // set up
+        const item1 = todoList.create(new TodoItem("turn the heating on!"))
+        const item2 = todoList.create(new TodoItem("Do the washing up"))
+        todoList.setComplete(item1.id)
+        const expected = [item1]
+  
+        // execute
+        const result = todoList.getByStatus("complete")
+  
+        // verify
+        expect(result).toEqual(expected)
+      })
+    })
 
-    // execute
-    const result = todoList.getByStatus("incomplete")
+    describe('Find item by id', () => {
+      it("finds item by id", () => {
+        // set up
+        const expected = {
+          id: 1,
+          text: "turn the heating on!",
+          status: "incomplete",
+          date: '15/12/2023'
+        }
+  
+        // execute
+        todoList.create(new TodoItem("turn the heating on!"))
+        const result = todoList.findBy(1)
+  
+        // verify
+        expect(result).toEqual(expected)
+      })
+  
+      it("findBy throws error if not found", () => {
+        // set up
+  
+        // execute, verify
+        expect(() => todoList.findBy(1)).toThrowError("Item not found")
+      })
+    })
+  
 
-    // verify
-    expect(result).toEqual(expected)
+    describe('Delete item by id', () => {
+      it("deletes item by id", () => {
+        // set up
+        const expected = {
+          id: 1,
+          text: "turn the heating on!",
+          status: "incomplete",
+          date: '15/12/2023'
+        }
+  
+        // execute
+        todoList.create(new TodoItem("turn the heating on!"))
+        const deletedItem = todoList.deleteBy(1)
+  
+        // verify
+        expect(deletedItem).toEqual(expected)
+        expect(todoList.showAll()).toEqual([])
+      })
+  
+      it("delete throws error if not found", () => {
+        // set up
+  
+        // execute, verify
+        expect(() => todoList.deleteBy(1)).toThrowError("Item not found")
+      })
+    })
+
+    describe('Delete item by id', () => {
+      it("deletes item by id", () => {
+        // set up
+        const expected = {
+          id: 1,
+          text: "turn the heating on!",
+          status: "incomplete",
+          date: '15/12/2023'
+        }
+  
+        // execute
+        todoList.create(new TodoItem("turn the heating on!"))
+        const deletedItem = todoList.deleteBy(1)
+  
+        // verify
+        expect(deletedItem).toEqual(expected)
+        expect(todoList.showAll()).toEqual([])
+      })
+  
+      it("delete throws error if not found", () => {
+        // set up
+  
+        // execute, verify
+        expect(() => todoList.deleteBy(1)).toThrowError("Item not found")
+      })
+    })
+
+    describe('Update item text', () => {
+      it('updateText finds the selected todo by id and updates the text to user input', () => {
+        // set
+        const expected = {
+          id: 1,
+          text: "Walk the dog",
+          status: "incomplete",
+          date: '15/12/2023'
+        }
+  
+        // execute
+        todoList.create(new TodoItem("turn the heating on!"))
+        const result = todoList.updateText(1, "Walk the dog")
+  
+        // verify
+        expect(result).toEqual(expected)
+      })
+  
+      it('updateText throws error if item is not found or text is invalid', () => {
+        // set
+  
+        // execute/verify
+        expect(() => todoList.updateText()).toThrowError("unable to process, make sure id is an Integer and text is a String")
+      })
+    })
   })
-
-  it("gets complete items", () => {
-    // set up
-    const item1 = todoList.create("turn the heating on!")
-    const item2 = todoList.create("Do the washing up")
-    todoList.setComplete(item1.id)
-    const expected = [item1]
-
-    // execute
-    const result = todoList.getByStatus("complete")
-
-    // verify
-    expect(result).toEqual(expected)
-  })
-
-  it("finds item by id", () => {
-    // set up
-    const item1 = todoList.create("turn the heating on!")
-    const expected = {
-      id: 1,
-      text: "turn the heating on!",
-      status: "incomplete"
-    }
-
-    // execute
-    const result = todoList.findBy(item1.id)
-
-    // verify
-    expect(result).toEqual(expected)
-  })
-
-  it("findBy throws error if not found", () => {
-    // set up
-
-    // execute, verify
-    expect(() => todoList.findBy(1)).toThrowError("Item not found")
-  })
-
-  it("deletes item by id", () => {
-    // set up
-    const item1 = todoList.create("turn the heating on!")
-    const expected = {
-      id: 1,
-      text: "turn the heating on!",
-      status: "incomplete"
-    }
-
-    // execute
-    const deletedItem = todoList.deleteBy(1)
-
-    // verify
-    expect(deletedItem).toEqual(expected)
-    expect(todoList.showAll()).toEqual([])
-  })
-
-  it("delete throws error if not found", () => {
-    // set up
-
-    // execute, verify
-    expect(() => todoList.deleteBy(1)).toThrowError("Item not found")
-  })
-})
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,23 +1,52 @@
+let id = 1
+
+const setId = (value) => {
+  id = value
+}
+
+class TodoItem {
+  constructor(str) {
+    this.id = id
+    this.text = str
+    this.status = 'incomplete'
+    this.date = new Date().toLocaleDateString()
+    setId(id +1)
+  }
+}
+
 class TodoList {
   constructor () {
     this.id = 0
     this.items = []
+    this.itemsShortText =[]
   }
 
-  create (str) {
-    this.id++
-    const item = { id: this.id, text: str, status: 'incomplete' }
-    this.items.push(item)
-    return item
+    create(item) {
+      const itemToCreate = { id: item.id, text: item.text, status: item.status, date: item.date }
+      this.items.push(itemToCreate)
+      return itemToCreate
   }
 
   showAll () {
-    return this.items
+    this.items.forEach(item => {
+      if (item.text.length > 20) {
+        item.text = `${item.text.substring(0, 20)}...`
+        this.itemsShortText.push(item)
+        return item
+      } else {
+        this.itemsShortText.push(item)
+        return item
+      }
+    }) 
+    return this.itemsShortText
   }
 
   setComplete (id) {
     const item = this.findBy(id)
-    item.status = 'complete'
+    if (item.status === 'incomplete') {
+      item.status = 'complete'
+    }
+    else item.status = 'incomplete'
     return item
   }
 
@@ -36,6 +65,26 @@ class TodoList {
     const index = this.items.indexOf(item)
     return this.items.splice(index, 1)[0]
   }
+
+  getByDate(date) {
+    const checkDateArr = this.items.filter(item => item.date === date)
+    // if array is empty, i.e. no valid date found
+    if (checkDateArr.length === 0) return 'Item not found, search by date format DD/MM/YYYY'
+    // else check if there are multiple todos w/ same date
+    else {
+      // only one todo - return todo with full length text
+      if (checkDateArr.length === 1) return checkDateArr
+      // multiple todos - return all todos with this date with shortened text
+      else return this.showAll().filter(item => item.date === date)
+    }
+  }
+
+  updateText(id, text) {
+    const itemToUpdate = this.items.find(item => item.id === id)
+    if (itemToUpdate === undefined || typeof text !== 'string') throw new Error ('unable to process, make sure id is an Integer and text is a String')
+    itemToUpdate.text = text
+    return itemToUpdate
+  }
 }
 
-module.exports = TodoList
+module.exports = {TodoList, TodoItem, setId}
