572624fb1fe0b41a36ee39091beb13b362e8adb4
SukunimiVinod <Vinodvinod19032022@gmail.com>
2023-12-31 13:28:10 +0200

    test passed

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,41 +1,49 @@
 class TodoList {
-  constructor () {
+  constructor() {
     this.id = 0
     this.items = []
   }
 
-  create (str) {
+  create(str) {
     this.id++
     const item = { id: this.id, text: str, status: 'incomplete' }
     this.items.push(item)
     return item
   }
 
-  showAll () {
+  showAll() {
     return this.items
   }
 
-  setComplete (id) {
+  setComplete(id) {
     const item = this.findBy(id)
     item.status = 'complete'
     return item
   }
 
-  getByStatus (status) {
-    return this.items.filter(item => item.status === status)
+  getByStatus(status) {
+    return this.items.filter((item) => item.status === status)
   }
 
-  findBy (id) {
-    const item = this.items.find(item => item.id === id)
+  findBy(id) {
+    const item = this.items.find((item) => item.id === id)
     if (item === undefined) throw new Error('Item not found')
     return item
   }
 
-  deleteBy (id) {
+  deleteBy(id) {
     const item = this.findBy(id)
     const index = this.items.indexOf(item)
     return this.items.splice(index, 1)[0]
   }
 }
 
+const todoList = new TodoList()
+
+todoList.create('homework')
+todoList.create('classwork')
+
+console.log(todoList.showAll())
+
+todoList.showAll()
 module.exports = TodoList
